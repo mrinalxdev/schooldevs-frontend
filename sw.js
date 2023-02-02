af6aa638c2c2788a0b3709c78b1da6a4aa379c9a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-02 12:29:19 +0530

    Done with the PWA issues for instalibility

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 self.addEventListener("install", e => {
-    console.log("Install !")
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll(['./', './src/main.css', './images/logo2.jpg'])
+        })
+    )
+})
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request)
+        })
+    )
+    
 })
