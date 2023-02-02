9abe3fbfc731d57a934b32dbd0593908b9ab36bd
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-02 11:53:47 +0530

    Added Service Worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js').then(registration => {
-        console.log("SW Registered!")
-        console.log(registration)
-    }).catch(error => {
-        console.log("SW Registration Failed")
-        console.log(error)
-
-    })
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    //registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}else {
+  console.log('No service-worker on this browser');
 }
