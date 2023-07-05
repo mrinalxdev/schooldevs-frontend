103e43491384b8eae72d0bd2ee02f55751f7f305
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-05 21:36:29 +0530

    Added New Data

--- client/src/components/QuizComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuizComponent = () => {
-  return (
-    <div>QuizComponent</div>
-  )
-}
-
-export default QuizComponent
--- client/src/components/Result.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Result = () => {
-  return (
-    <div>Result</div>
-  )
-}
-
-export default Result
