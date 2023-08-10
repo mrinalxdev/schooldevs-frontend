5677e3f4b9febd3eada17825d8f4f8b6d809edf9
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-11 01:43:52 +0530

    Added : Courses Section and made a Scroll Animation to the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HiMenuAlt1, HiX } from "react-icons/hi";
 import { Link } from 'react-router-dom'
 import { navLinks } from "../../Data";
@@ -7,10 +7,20 @@ import MobileNavLinks from "./MobileNavLinks";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState(null)
+
+  useEffect(() => {
+    const scrollActive = () => {
+      setActive(window.scrollY > 20)
+    }
+    window.addEventListener("scroll",scrollActive)
+    return () => window.removeEventListener("scroll", scrollActive)
+  }, [active])
+
   return (
-    <div className="fixed w-full top-0 left-0 z-20">
+    <div className={`${active ? "shadow-lg bg-Solitude" : ""} fixed w-full top-0 left-0 z-20`}>
       <div className="">
-        <div className="container py-4 mx-auto flex items-center justify-between px-2">
+        <div className={`${active ? "py-2 transition-all duration-300" : "py-4"} container py-4 mx-auto flex items-center justify-between px-2`}>
           <div className="flex items-center gap-4">
             <HiMenuAlt1
               className="text-3xl sm:hidden cursor-pointer"
--- src/components/container/Courses/Courses.jsx
+++ src/components/container/Courses/Courses.jsx
@@ -23,8 +23,8 @@ const Courses = () => {
         })}
       </div>
       <div className="text-xl font-bold mt-32">Most Popular Courses</div>
-      <div className="mt-12 overflow-x-hidden w-full">
-        <div className="flex gap-8 md-w-full sm:w-[170%] xs:W-[340%] w-[480%]">
+      <div className="mt-12 overflow-x-hidden w-full relative">
+        <div className="flex gap-8 md:w-full sm:w-[170%] xs:W-[340%] w-[480%] animate-slide">
           {courses.map((course) => {
             return <Course key={course.id} {...course} />;
           })}
